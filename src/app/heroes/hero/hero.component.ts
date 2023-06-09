6d15dcc1ceb9e4eb2cd8d26691600a3156a55ec3
Ramiro <jesusramiro_valenzuela@outlook.com>
2023-06-09 07:58:38 -0700

    42. getters y funciones

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name: string = 'Ironman';
   public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
 }
