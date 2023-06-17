e9f6a6d9bdc0d11ac2a5b5f1528759fbe5c6646e
Ramiro <jesusramiro_valenzuela@outlook.com>
2023-06-17 14:20:14 -0700

    67. Servicios

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -14,4 +14,14 @@ export class ListComponent {
       power: 10,
     },
   ];
+
+  @Output()
+  public idCharacter: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number): void {
+    // TODO: Emitir el ID del personaje
+    console.log(index);
+
+    this.idCharacter.emit(index);
+  }
 }
